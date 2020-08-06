09f6baaf9ceebf44152f9a518dd185bd2473f6a7
steve10live <38049797+steve10live@users.noreply.github.com>
2020-08-07 02:01:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-import objects
-objects.send_dev_message('ебать работает ребятя', good=True)
+import heroku3
+import os
+import _thread
+from version import version as new_version
+
+
+def drop():
+    if os.environ.get('version') != new_version:
+        if os.environ.get('api'):
+            connection = heroku3.from_key(os.environ.get('api'))
+            for app in connection.apps():
+                config = app.config()
+                config['version'] = new_version
+
+
+if __name__ == '__main__':
+    _thread.start_new_thread(drop, ())
