b021a98a6895bed89191f1475c908df13497f706
steve10live <38049797+steve10live@users.noreply.github.com>
2020-08-07 03:08:16 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-import heroku3
 import os
-import _thread
+import sys
 from time import sleep
-from version import version as new_version
 
-while True:
-    sleep(10)
-    print('old', os.environ.get('version'))
-    print('new', new_version)
-    if os.environ.get('version') != new_version:
-        if os.environ.get('api'):
-            connection = heroku3.from_key(os.environ.get('api'))
-            for app in connection.apps():
-                config = app.config()
-                config['version'] = new_version
-    sleep(10)
-    if os.environ.get('count'):
-        if os.environ.get('api'):
-            connection = heroku3.from_key(os.environ.get('api'))
-            for app in connection.apps():
-                config = app.config()
-                config['count'] = int(os.environ.get('count')) + 1
-    _thread.exit()
+os.system('python setup.py sdist')
+os.system('twine upload --repository-url https://upload.pypi.org/legacy/ dist/* '
+          '--username evolvestin --password NAEzZ_4sF$9c*#@MUX$H7X8^k=vkRB')
+sleep(3600)
