4b60e313bd136b75a6648b5993ca93a1a5cb99ab
steve10live <38049797+steve10live@users.noreply.github.com>
2020-11-02 13:54:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-import os
-import sys
+import gspread
 from time import sleep
 
-os.system('python setup.py sdist')
-os.system('twine upload --repository-url https://upload.pypi.org/legacy/ dist/* '
-          '--username evolvestin --password NAEzZ_4sF$9c*#@MUX$H7X8^k=vkRB')
-sleep(3600)
+while True:
+    acc = gspread.service_account('worker1.json')
+    files = acc.list_spreadsheet_files()
+    for i in files:
+        print(i)
+    sleep(100)
