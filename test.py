5208ba65ee0e4f81cc0aea6b73a0e9da70e917a5
steve10live <38049797+steve10live@users.noreply.github.com>
2020-11-02 22:13:05 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from time import sleep
 
 while True:
     file = open('XSTORAGE1.json').read()
+    XSTORAGE1
     print(file)
     #acc = gspread.service_account('worker1.json')
     #files = acc.list_spreadsheet_files()
