df60bf88e4207a5dbf55d5271531a13468fa69d6
steve10live <38049797+steve10live@users.noreply.github.com>
2020-11-02 22:29:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import gspread
+import os
+import _thread
 from time import sleep
+directory = os.listdir('.')
+os.environ["DEBUSSY"] = 'l'
+
+m = os.environ.keys()
+for i in m:
+    print(i, '-', os.environ.get(i))
+
+_thread.exit()
 
 while True:
-    file = open('XSTORAGE1.json').read()
-    XSTORAGE1
-    print(file)
     #acc = gspread.service_account('worker1.json')
     #files = acc.list_spreadsheet_files()
     #for i in files:
     #    print(i)
     sleep(100)
+
