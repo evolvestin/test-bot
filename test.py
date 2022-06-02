116afbd88b4d911254198441bcdf19a58c3c0d8a
evolvestin <evolvestin@gmail.com>
2022-06-02 13:01:24 +0300

    ok

--- test.py
+++ test.py
@@ -6,12 +6,17 @@ import subprocess
 import sys
 from telegraph import upload
 from time import sleep
+from datetime import datetime
 os.environ["DEBUSSY"] = 'l'
+stamp = datetime.now().timestamp()
+
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-    sleep(1000)
+
 
 while True:
     os.system("echo Hello from the other side!")
-    install('e-objects')
+    install('e-objects>=2.10.1')
+    print('Ушло на установку одного модуля', datetime.now().timestamp() - stamp, 'секунд')
+    sleep(1000)
