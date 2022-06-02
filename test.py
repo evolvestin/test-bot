75f4698389d244fee44b5767f921196543b897cb
evolvestin <evolvestin@gmail.com>
2022-06-02 14:26:02 +0300

    ok

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import gspread
+import re
 import os
+from bs4 import BeautifulSoup
+import requests
 import _thread
 import telebot
 import subprocess
@@ -9,30 +12,39 @@ from time import sleep
 from datetime import datetime
 os.environ["DEBUSSY"] = 'l'
 stamp = datetime.now().timestamp()
+downloads = []
 
-f = """emoji==1.7.0
-aiogram==2.20
-Pillow==9.1.0
-heroku3==4.2.3
-gspread==3.6.0
-Unidecode==1.2.0
-requests>=2.25.1
-telegraph==1.4.0
-selenium==3.141.0
-GitPython==3.1.17
-beautifulsoup4==4.9.3
-pyTelegramBotAPI==4.5.1
-google-api-python-client==1.10.0"""
+with open('go.txt') as file:
+    requirements = file.read()
 
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+def install(pack):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", pack])
+
+
+for package in requirements.split('\n'):
+    search = re.search('[!~>=]=(.*)', package)
+    version = search.group(1) if search else None
+    library_name = re.sub('[~>=]=.*', '', package)
+    query = BeautifulSoup(requests.get(f'https://pypi.org/simple/{library_name}/').text, 'html.parser')
+    links = query.find_all('a', text=f"{library_name}-{version}.tar.gz") if version else query.find_all('a')
+    download_link = links[-1].get('href') if links else None
+    if download_link:
+        print(download_link)
+        download = requests.get(download_link, stream=True)
+        with open(f'{library_name}-{version}.tar.gz', 'wb') as f:
+            for chunk in download:
+                f.write(chunk)
+        downloads.append(f'{library_name}-{version}.tar.gz')
+
+print('Все файлы выкачаны', downloads, datetime.now().timestamp() - stamp)
+
+stamp = datetime.now().timestamp()
 
 
 while True:
-    go = f.split('\n')
     os.system("echo Hello from the other side!")
-    for i in go:
+    for i in downloads:
         install(i)
-    print(f'Ушло на установку {len(go)} модулей', datetime.now().timestamp() - stamp, 'секунд')
+    print(f'Ушло на установку {len(downloads)} модулей', datetime.now().timestamp() - stamp, 'секунд')
     sleep(1000)
