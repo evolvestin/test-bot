dc72965c7af69e085b8cabb10321ac743827aaf0
evolvestin <evolvestin@gmail.com>
2022-06-02 13:23:28 +0300

    ok

--- test.py
+++ test.py
@@ -9,7 +9,20 @@ from time import sleep
 from datetime import datetime
 os.environ["DEBUSSY"] = 'l'
 stamp = datetime.now().timestamp()
-test = ['emoji==1.7.0', 'Pillow>=9.1.0', 'gspread>=3.6.0', 'requests>=2.20.0', 'telegraph==1.4.1', 'GitPython==3.1.17']
+
+f = """emoji==1.7.0
+aiogram==2.20
+Pillow==9.1.0
+heroku3==4.2.3
+gspread==3.6.0
+Unidecode==1.2.0
+requests>=2.25.1
+telegraph==1.4.0
+selenium==3.141.0
+GitPython==3.1.17
+beautifulsoup4==4.9.3
+pyTelegramBotAPI==4.5.1
+google-api-python-client==1.10.0"""
 
 
 def install(package):
@@ -17,8 +30,9 @@ def install(package):
 
 
 while True:
+    go = f.split('\n')
     os.system("echo Hello from the other side!")
-    for i in test:
+    for i in go:
         install(i)
-    print('Ушло на установку 6 модулей', datetime.now().timestamp() - stamp, 'секунд')
+    print(f'Ушло на установку {len(go)} модулей', datetime.now().timestamp() - stamp, 'секунд')
     sleep(1000)
