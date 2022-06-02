9a17f21bc371e109509a2b0afd0bd8725cb06364
evolvestin <38049797+evolvestin@users.noreply.github.com>
2022-06-02 12:55:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,16 @@ import gspread
 import os
 import _thread
 import telebot
+import subprocess
+import sys
 from telegraph import upload
 from time import sleep
 os.environ["DEBUSSY"] = 'l'
 
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    sleep(1000)
+
 while True:
     os.system("echo Hello from the other side!")
-    sleep(1000)
+    install('e-objects')
